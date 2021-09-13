d75382d2f3249b27c895d06de936f213a8ff2afa
mvuja <markowujanovic@gmail.com>
2021-09-13 14:44:31 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+
+
 // CARDS
 
 const line = $('.line')
